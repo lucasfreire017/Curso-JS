766dd049a7f30a9505e7ee229dad925cef27c4d8
Lucas Freire <lucas.kiko.kl@gmail.com>
2020-07-19 16:25:33 -0300

    Initial commit

---
+++ aula13/ambiente.js
@@ -0,0 +1,14 @@
+var c = 1
+
+console.log('Utilizando Do')
+do{
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 6)
+                                                   
+var d = 1
+console.log('Utilizando While')
+while(d <= 6){
+    console.log(`Passo ${d}`)
+    d++
+}
