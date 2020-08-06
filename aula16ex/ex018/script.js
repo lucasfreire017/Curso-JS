9bf246540dfa22c01ad10dbb48a81f89896afbc7
Lucas Freire <lucas.kiko.kl@gmail.com>
2020-08-06 00:39:15 -0300

    Adcionado novas funções

--- aula16ex/ex018/script.js
+++ aula16ex/ex018/script.js
@@ -3,6 +3,7 @@ let lista = document.querySelector('select#flista')
 let res = document.querySelector('div#res')
 let valores = []
 
+// Verifica se o número está entre 1 e 100
 function isNumero(n){
     if(Number(n) >= 1 && Number(n) <= 100){
         return true
@@ -11,6 +12,7 @@ function isNumero(n){
     }
 }
 
+// Não ter repetição de valores na lista
 function inLista(n, l){
     if(l.indexOf(Number(n)) != -1){
         return true
@@ -18,11 +20,18 @@ function inLista(n, l){
         return false
     }
 }
-
+// Adcionar os valores 
 function adcionar(){
     if(isNumero(num.value) && !inLista(num.value, valores)){
-       window.alert('Tudo Ok') 
+       let item = document.createElement('option')
+       valores.push(num.value)
+       item.text = `Valor ${num.value} adicionado`
+       lista.appendChild(item)
     } else {
         window.alert('Valor inválido ou já encontrado na lista.')
     }
+}
+
+function final(){
+    res.innerHTML = `<p>Ao todo temos ${valores.length} númeoros cadastrados.</p>`
 }
